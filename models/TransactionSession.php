0fd59e8b79f80f6e93cc8eaca42703d81f94ea48
AxelRj13 <axel.raharja1@gmail.com>
2018-11-22 18:38:09 +0700

    Penambahan model transaction item dan transaction session + penyesuaian

--- models/TransactionSession.php
+++ models/TransactionSession.php
@@ -5,33 +5,32 @@ namespace core\models;
 use Yii;
 
 /**
- * This is the model class for table "business_product".
+ * This is the model class for table "transaction_session".
  *
  * @property int $id
- * @property int $business_id
- * @property string $name
- * @property string $description
- * @property int $price
- * @property string $image
- * @property bool $not_active
+ * @property string $customer_name
+ * @property string $note
+ * @property int $total_price
  * @property string $created_at
  * @property int $user_created
  * @property string $updated_at
  * @property int $user_updated
+ * @property int $user_ordered
+ * @property bool $is_closed
  *
- * @property Business $business
+ * @property TransactionItem[] $transactionItems
  * @property User $userCreated
  * @property User $userUpdated
- * @property TransactionItem[] $transactionItems
+ * @property User $userOrdered
  */
-class BusinessProduct extends \sybase\SybaseModel
+class TransactionSession extends \sybase\SybaseModel
 {
     /**
      * @inheritdoc
      */
     public static function tableName()
     {
-        return 'business_product';
+        return 'transaction_session';
     }
 
     /**
@@ -40,17 +39,16 @@ class BusinessProduct extends \sybase\SybaseModel
     public function rules()
     {
         return [
-            [['business_id', 'name', 'price'], 'required'],
-            [['business_id', 'price', 'user_created', 'user_updated'], 'default', 'value' => null],
-            [['business_id', 'price', 'user_created', 'user_updated'], 'integer'],
-            [['description'], 'string'],
-            [['not_active'], 'boolean'],
+            [['note'], 'string'],
+            [['total_price', 'user_created', 'user_updated', 'user_ordered'], 'default', 'value' => null],
+            [['total_price', 'user_created', 'user_updated', 'user_ordered'], 'integer'],
             [['created_at', 'updated_at'], 'safe'],
-            [['name'], 'string', 'max' => 64],
-            [['image'], 'string', 'max' => 128],
-            [['business_id'], 'exist', 'skipOnError' => true, 'targetClass' => Business::className(), 'targetAttribute' => ['business_id' => 'id']],
+            [['user_ordered'], 'required'],
+            [['is_closed'], 'boolean'],
+            [['customer_name'], 'string', 'max' => 64],
             [['user_created'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_created' => 'id']],
             [['user_updated'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_updated' => 'id']],
+            [['user_ordered'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_ordered' => 'id']],
         ];
     }
 
@@ -61,25 +59,24 @@ class BusinessProduct extends \sybase\SybaseModel
     {
         return [
             'id' => Yii::t('app', 'ID'),
-            'business_id' => Yii::t('app', 'Business ID'),
-            'name' => Yii::t('app', 'Name'),
-            'description' => Yii::t('app', 'Description'),
-            'price' => Yii::t('app', 'Price'),
-            'image' => Yii::t('app', 'Image'),
-            'not_active' => Yii::t('app', 'Not Active'),
+            'customer_name' => Yii::t('app', 'Customer Name'),
+            'note' => Yii::t('app', 'Note'),
+            'total_price' => Yii::t('app', 'Total Price'),
             'created_at' => Yii::t('app', 'Created At'),
             'user_created' => Yii::t('app', 'User Created'),
             'updated_at' => Yii::t('app', 'Updated At'),
             'user_updated' => Yii::t('app', 'User Updated'),
+            'user_ordered' => Yii::t('app', 'User Ordered'),
+            'is_closed' => Yii::t('app', 'Is Closed'),
         ];
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getBusiness()
+    public function getTransactionItems()
     {
-        return $this->hasOne(Business::className(), ['id' => 'business_id']);
+        return $this->hasMany(TransactionItem::className(), ['transaction_session_id' => 'id']);
     }
 
     /**
@@ -101,8 +98,8 @@ class BusinessProduct extends \sybase\SybaseModel
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getTransactionItems()
+    public function getUserOrdered()
     {
-        return $this->hasMany(TransactionItem::className(), ['business_product_id' => 'id']);
+        return $this->hasOne(User::className(), ['id' => 'user_ordered']);
     }
 }
