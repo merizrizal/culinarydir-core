e16008952a1e0c0f9a712bacc68cff8b26889822
Herryawan Wiguna <wigunaherryawan@gmail.com>
2019-01-01 17:55:49 +0700

    Pernambahan payment method

---
+++ models/PaymentMethod.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace core\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "payment_method".
+ *
+ * @property int $id
+ * @property string $payment_name
+ * @property string $method
+ * @property bool $not_active
+ * @property string $created_at
+ * @property int $user_created
+ * @property string $updated_at
+ * @property int $user_updated
+ *
+ * @property User $userCreated
+ * @property User $userUpdated
+ */
+class PaymentMethod extends \sybase\SybaseModel
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'payment_method';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['payment_name', 'method'], 'required'],
+            [['method'], 'string'],
+            [['not_active'], 'boolean'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['user_created', 'user_updated'], 'default', 'value' => null],
+            [['user_created', 'user_updated'], 'integer'],
+            [['payment_name'], 'string', 'max' => 32],
+            [['user_created'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_created' => 'id']],
+            [['user_updated'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_updated' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'payment_name' => Yii::t('app', 'Payment Name'),
+            'method' => Yii::t('app', 'Method'),
+            'not_active' => Yii::t('app', 'Not Active'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'user_created' => Yii::t('app', 'User Created'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'user_updated' => Yii::t('app', 'User Updated'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserCreated()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_created']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserUpdated()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_updated']);
+    }
+}
