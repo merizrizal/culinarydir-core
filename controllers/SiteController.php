246663e119fbe899c32230785fea24e5bf477d01
Mei Rizal <meriz.rizal@gmail.com>
2019-01-18 18:58:40 +0700

    perbaikan script migrasi

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -115,7 +115,7 @@ class SiteController extends Controller
             FROM information_schema.table_constraints tc 
             JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name) 
             JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
-            WHERE constraint_type = \'PRIMARY KEY\' AND ccu.column_name NOT IN(\'business_id\', \'registry_business_id\', \'user_id\') AND tc.table_name NOT IN(\'status_approval\');
+            WHERE constraint_type = \'PRIMARY KEY\' AND ccu.column_name NOT IN(\'business_id\', \'registry_business_id\', \'transaction_session_id\', \'user_id\') AND tc.table_name NOT IN(\'status_approval\');
         ')->queryAll();
             
         $selectTable = [];
