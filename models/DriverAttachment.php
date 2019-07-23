0c55aca62114b0a4d49a0c3175bab3ae8899214c
Muhammad Husni Farid <husnifa02@gmail.com>
2019-07-23 17:51:48 +0700

    Penambahan dan perbaikan model

--- models/DriverAttachment.php
+++ models/DriverAttachment.php
@@ -4,29 +4,27 @@ namespace core\models;
 
 
 /**
- * This is the model class for table "driver_criteria".
+ * This is the model class for table "driver_attachment".
  *
  * @property string $id
- * @property string $person_as_driver_id
- * @property string $criteria_name
  * @property string $type
+ * @property string $file_name
  * @property string $created_at
  * @property string $user_created
  * @property string $updated_at
  * @property string $user_updated
  *
- * @property PersonAsDriver $personAsDriver
  * @property User $userCreated
  * @property User $userUpdated
  */
-class DriverCriteria extends \sybase\SybaseModel
+class DriverAttachment extends \sybase\SybaseModel
 {
     /**
      * @inheritdoc
      */
     public static function tableName()
     {
-        return 'driver_criteria';
+        return 'driver_attachment';
     }
 
     /**
@@ -35,13 +33,11 @@ class DriverCriteria extends \sybase\SybaseModel
     public function rules()
     {
         return [
-            [['person_as_driver_id', 'criteria_name', 'type'], 'required'],
-            [['type'], 'string'],
+            [['type', 'file_name'], 'required'],
+            [['file_name'], 'string'],
             [['created_at', 'updated_at'], 'safe'],
-            [['id', 'person_as_driver_id', 'user_created', 'user_updated'], 'string', 'max' => 32],
-            [['criteria_name'], 'string', 'max' => 50],
+            [['id', 'type', 'user_created', 'user_updated'], 'string', 'max' => 32],
             [['id'], 'unique'],
-            [['person_as_driver_id'], 'exist', 'skipOnError' => true, 'targetClass' => PersonAsDriver::className(), 'targetAttribute' => ['person_as_driver_id' => 'person_id']],
             [['user_created'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_created' => 'id']],
             [['user_updated'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_updated' => 'id']],
         ];
@@ -54,22 +50,13 @@ class DriverCriteria extends \sybase\SybaseModel
     {
         return [
             'id' => \Yii::t('app', 'ID'),
-            'person_as_driver_id' => \Yii::t('app', 'Person As Driver ID'),
-            'criteria_name' => \Yii::t('app', 'Criteria Name'),
             'type' => \Yii::t('app', 'Type'),
+            'file_name' => \Yii::t('app', 'File Name'),
             'created_at' => \Yii::t('app', 'Created At'),
             'user_created' => \Yii::t('app', 'User Created'),
             'updated_at' => \Yii::t('app', 'Updated At'),
             'user_updated' => \Yii::t('app', 'User Updated'),
         ];
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getPersonAsDriver()
-    {
-        return $this->hasOne(PersonAsDriver::className(), ['person_id' => 'person_as_driver_id']);
     }
 
     /**
